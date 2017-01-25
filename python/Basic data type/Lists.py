e0787325788d1cd6276bcbad48c68446d494420f
Rahul <rahulrock0082@gmail.com>
2017-01-25 06:46:56 +0530

    Create Lists.py

---
+++ python/Basic data type/Lists.py
@@ -0,0 +1,11 @@
+n = input()
+l = []
+for _ in range(n):
+    s = raw_input().split()
+    cmd = s[0]
+    args = s[1:]
+    if cmd !="print":
+        cmd += "("+ ",".join(args) +")"
+        eval("l."+cmd)
+    else:
+        print l
