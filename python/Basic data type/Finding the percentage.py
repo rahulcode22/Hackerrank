3bb58e66b18184aaba0e7b522a02f514bd0bbb31
Rahul <rahulrock0082@gmail.com>
2017-01-25 16:30:41 +0530

    Create Finding the percentage.py

---
+++ python/Basic data type/Finding the percentage.py
@@ -0,0 +1,8 @@
+n = int(raw_input())
+mydict = {}
+for line in range(n):
+    info = raw_input().split(" ")
+    score = map(float, info[1:])
+    mydict[info[0]] = sum(score) / float(len(score))
+
+print "%.2f" % round(mydict[raw_input()],2) 
