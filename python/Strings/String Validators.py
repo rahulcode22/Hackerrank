92d2ed08a70c1364b41feb421e7e3db22b09dc5b
Rahul <rahulrock0082@gmail.com>
2017-01-25 16:35:35 +0530

    Create String Validators.py

---
+++ python/Strings/String Validators.py
@@ -0,0 +1,9 @@
+
+s=raw_input()
+
+print any(c.isalnum()  for c in s)
+print any(c.isalpha()  for c in s)
+print any(c.isdigit()  for c in s)
+print any(c.islower()  for c in s)
+print any(c.isupper()  for c in s)
+
