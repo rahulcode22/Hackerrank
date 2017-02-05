4f3786799944b125ca157d6ccc0863cecfeeeef7
Rahul <rahulrock0082@gmail.com>
2017-02-05 20:24:41 +0530

    Create Get Node Value.py

---
+++ Data Structures/Linkedlist/Get Node Value.py
@@ -0,0 +1,14 @@
+def GetNode(head, position):
+    index=0
+    temp=head
+    res=head
+    while(temp is not None):
+        temp=temp.next
+        
+        if (index > position):
+            
+            res=res.next
+        index=index+1
+    return res.data
+    
+ 
