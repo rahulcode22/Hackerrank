8c37d0472c1d0c40bc4185acf00305db341bae9c
Rahul <rahulrock0082@gmail.com>
2017-02-19 10:36:09 +0530

    Create Collections.deque().py

---
+++ python/Collections/Collections.deque().py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
+d = deque()
+for i in range(int(raw_input())):
+    s = raw_input().split()
+    if s[0] == 'append':
+        d.append(s[1])
+    elif s[0] == 'appendleft':
+        d.appendleft(s[1])
+    elif s[0] == 'pop':
+        d.pop()
+    else:
+        d.popleft()
+print " ".join(d) 
