84aa79637049a10e2530f9107c8038d5ec326e37
Rahul <rahulrock0082@gmail.com>
2017-02-19 10:37:57 +0530

    Create DefaultDict Tutorial.py

---
+++ python/Collections/DefaultDict Tutorial.py
@@ -0,0 +1,18 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+d = defaultdict(list)
+list1=[]
+
+n, m = map(int,raw_input().split())
+
+for i in range(0,n):
+    d[raw_input()].append(i+1) 
+
+for i in range(0,m):
+    list1=list1+[raw_input()]  
+
+for i in list1: 
+    if i in d:
+        print " ".join( map(str,d[i]) )
+    else:
+        print -1
