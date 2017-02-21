efcb8c05d1cd9068541c1fa46f4d958a53e4d494
Rahul <rahulrock0082@gmail.com>
2017-02-21 07:27:22 +0530

    Create Binary Search Tree : Lowest Common Ancestor.py

---
+++ Data Structures/Tree/Binary Search Tree : Lowest Common Ancestor.py
@@ -0,0 +1,10 @@
+node * lca(node * root, int n1,int n2)
+{
+
+if (root==NULL) return NULL;
+ if (root->data > n1 && root->data > n2)
+        return lca(root->left, n1, n2);
+     if (root->data < n1 && root->data < n2)
+        return lca(root->left, n1, n2);
+    return root;
+}
