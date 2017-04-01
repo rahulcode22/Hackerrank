b2f6f1c223b6f92c29adc4356e0914650e918a98
Rahul <rahulrock0082@gmail.com>
2017-04-01 19:18:17 +0530

    Create Compare-the-Triplets.cpp

---
+++ Warmup/Compare-the-Triplets.cpp
@@ -0,0 +1,43 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int a0;
+    int a1;
+    int a2;
+    int ap=0,ab=0;
+    cin >> a0 >> a1 >> a2;
+    int b0;
+    int b1;
+    int b2;
+    cin >> b0 >> b1 >> b2;
+    if(a0!=b0) {a0>b0 ? ap+=1 :ab+=1;}
+    if(a1!=b1) {a1>b1 ? ap+=1 :ab+=1;}
+    if(a2!=b2) {a2>b2 ? ap+=1 :ab+=1;}
+    cout<<ap<<" "<<ab;
+    
+    return 0;
+}
