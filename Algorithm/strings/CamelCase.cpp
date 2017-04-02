671ec560dc4d03e627e6640a4c378de5b0be59c2
Rahul <rahulrock0082@gmail.com>
2017-04-02 16:52:13 +0530

    Create CamelCase.cpp

---
+++ Algorithm/strings/CamelCase.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string s;
+    cin >> s;
+    cin.get();
+    int count=1;
+    for(int i=0;i<s.length();i++)
+        {
+            
+        if(isupper(s[i]))
+            {count+=1;}
+        }
+    cout<<count<<endl;
+    return 0;
+}
