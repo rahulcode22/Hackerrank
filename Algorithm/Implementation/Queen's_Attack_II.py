dc14111696def0f65eb6746877bc25da04d78443
Rahul <rahulrock0082@gmail.com>
2017-05-02 10:30:20 +0530

    Create Queen's_Attack_II.py

---
+++ Algorithm/Implementation/Queen's_Attack_II.py
@@ -0,0 +1,31 @@
+
+import sys
+
+
+n,k = raw_input().strip().split(' ')
+n,k = [int(n),int(k)]
+rQueen,cQueen = raw_input().strip().split(' ')
+rQueen,cQueen = [int(rQueen),int(cQueen)]
+s = set()
+
+for a0 in xrange(k):
+    rObstacle,cObstacle = raw_input().strip().split(' ')
+    rObstacle,cObstacle = [int(rObstacle),int(cObstacle)]
+    
+    s.add((rObstacle, cObstacle))
+#arrays for storing movement of queen (diagonal,up,down,diagonal-left,diagonal-right etc   
+mx = [0, 1, 1, 1, 0, -1, -1, -1]
+my = [-1, -1, 0, 1, 1, 1, 0, -1]
+
+ans = 0
+for i in xrange(8):
+    cur_y, cur_x = rQueen, cQueen
+    while 0 < cur_y <= n and 0 < cur_x <= n:
+        if (cur_y, cur_x) not in s:
+            ans += 1
+        else:
+            break
+        cur_y += my[i]
+        cur_x += mx[i]
+    ans -= 1
+print ans
